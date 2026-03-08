7890b551e44dfcd274b4a9cefc15fa0ace124b3b
Krystal <krystal.francois@hotmail.com>
2026-03-08 17:26:04 +0100

    cpp07 ex01 fini

--- CPP/CPP_07/ex01/main.cpp
+++ CPP/CPP_07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: krfranco <krfranco@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/07 14:49:56 by krfranco          #+#    #+#             */
-/*   Updated: 2026/03/07 15:46:00 by krfranco         ###   ########.fr       */
+/*   Updated: 2026/03/08 17:25:15 by krfranco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ void printElement(E &e)
 	std::cout << e << std::endl;
 }
 
+template <typename E>
 void plusTen(E &e)
 {
 	e += 10;
@@ -26,7 +27,27 @@ void plusTen(E &e)
 
 int main()
 {
-	int array1[3] = {4, 5, 6};
-	std::string array2[3] = {"yes", "no", "maybe"};
+	int int_array[3] = {4, 5, 6};
+    std::cout << "test for int: " << std::endl;
+    ::iter(int_array, 3, printElement<int>);
+    std::cout << "plusTen: " << std::endl;
+    ::iter(int_array, 3, plusTen<int>);
+    ::iter(int_array, 3, printElement<int>);
+
+  
+	std::string string_array[3] = {"yes", "no", "maybe"};
+	std::cout << "\ntest for string: " << std::endl;
+    ::iter(string_array, 3, printElement<std::string>);
+
+    double double_array[3] = {1.2, 3.4 ,5.6};
+    std::cout << "\ntest for double: " << std::endl;
+    ::iter(double_array, 3, printElement<double>);
+    std::cout << "plusTen: " << std::endl;
+    ::iter(double_array, 3, plusTen<double>);
+    ::iter(double_array, 3, printElement<double>);
+
+    const int d[] = {1, 2 ,3};
+    std::cout << "\ntest for const int: " << std::endl;
+    ::iter(d, 3, printElement<const int>);
 	return 0;
 }
