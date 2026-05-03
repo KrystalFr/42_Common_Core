c7cc7d3f17cafb766d770e1b5706cbb5b6d8b714
Krystal <krystal.francois@hotmail.com>
2026-05-03 18:02:06 +0200

    cpp08 done

---
+++ CPP/CPP_08/ex02/MutantStack.hpp
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: krfranco <krfranco@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/03 14:59:39 by krfranco          #+#    #+#             */
+/*   Updated: 2026/05/03 18:01:53 by krfranco         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#include <stack>
+
+template<typename T>
+class MutantStack : public std::stack<T>
+{
+	public:
+		typedef typename std::stack<T>::container_type::iterator iterator;
+		typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+
+	MutantStack(){}
+	MutantStack(const MutantStack &other) : std::stack<T>(other){}
+	MutantStack &operator=(const MutantStack &other)
+	{
+		if (this != &other)
+		{
+			std::stack<T>::operator=(other);
+		}
+		return *this;
+	}
+	~MutantStack(){}
+
+	//c est le container interne protégé dans std::stack, mais avec ma classe custom je peux y accéder et l'utiliser
+	iterator begin(){return this->c.begin();}
+	iterator end(){return this->c.end();}
+
+	const_iterator begin() const {return this->c.begin();}
+	const_iterator end() const {return this->c.end();}
+};
+#endif
