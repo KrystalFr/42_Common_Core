315144841ddb0bd384d0f16d078a4df29aa6734c
krfranco <krfranco@f2r8s7.paris.42.school>
2026-05-23 15:23:27 +0200

    wasting my time

--- CPP/CPP_09/ex00/main.cpp
+++ CPP/CPP_09/ex00/main.cpp
@@ -6,9 +6,13 @@
 /*   By: krfranco <krfranco@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/03 21:54:22 by krfranco          #+#    #+#             */
-/*   Updated: 2026/05/04 02:36:45 by krfranco         ###   ########.fr       */
+/*   Updated: 2026/05/23 12:51:49 by krfranco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <fstream>
+#include <iostream>
+#include <string>
 
 int main(int ac, char **av)
 {
@@ -18,12 +22,14 @@ int main(int ac, char **av)
 		return 1;
 	}
 
-	std::string inputFile = av[1];
-
-	std::ifstream inFile(inputFile.c_str());
-	if (!inFile.is_open())
+	std::string input = av[1];
+	std::ifstream infile(input.c_str());
+	
+	if (!infile.is_open())
 	{
 		std::cerr << "Error: could not open file." << std::endl;
+		return 1;
 	}
 	
+	return 0;
 }
